d6a0dec75ff3548d3fb55c4a6b873bcf8664b7b5
Meriane Demoliner <merianedemoliner@gmail.com>
2020-02-11 00:40:45 -0300

    added connection object and conversion to celsius

--- weather-data-scrapper.py
+++ weather-data-scrapper.py
@@ -1,9 +1,14 @@
-#https://www.wunderground.com/weather/br/nova-santa-rita
 import requests, bs4
-import mysql.connector
-from mysql.connector import Error
+from mysql.connector import MySQLConnection, Error
+from weatherdb_config import read_db_config
 
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) / 1.8
+
+# target cities
 cities = ['nova-santa-rita', 'canoas', 'novo-hamburgo', 'sao-leopoldo', 'parobe', 'taquara', 'tres-coroas', 'igrejinha', 'rolante', 'riozinho', 'caraa', 'santo-antonio-da-patrulha']
+
+# css selectors, relevant for scrapping
 temperature_css_selector = '.current-temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(1)'
 humidity_css_selector = ' .wu-unit-humidity > span:nth-child(1)'
 clouds_css_selector = 'span.wx-value:nth-child(1)'
@@ -13,11 +18,12 @@ for city in cities:
     html_page = requests.get('https://www.wunderground.com/weather/br/' + city)
     html_parsed = bs4.BeautifulSoup(html_page.text, 'html.parser')
     temperature_f = html_parsed.select_one(temperature_css_selector)
+    temperature_c = convert_fahrenheit_to_celsius(float(temperature_f.text))
     humidity = html_parsed.select_one(humidity_css_selector)
     clouds = html_parsed.select_one(clouds_css_selector)
     precipitation = html_parsed.select_one(precipitation_css_selector)    
     
-    print("City: " + city +  "\tTemperature: " + temperature_f.text + "F\tHumidity: " + humidity.text + "%\tClouds: " + clouds.text + "\tPrecipitation: " + precipitation.text)
+    print("City: " + city +  "\tTemperature: " + temperature_f.text + "F\tTemperature(C): " + str(temperature_c) + "C\tHumidity: " + humidity.text + "%\tClouds: " + clouds.text + "\tPrecipitation: " + precipitation.text)
 
 # def connect():
 #     """ Connect to MySQL database """
@@ -37,8 +43,9 @@ for city in cities:
 #         if connection is not None and connection.is_connected():
 #             connection.close()
 
+
 # def insert_data_by_city(city, temperature_c, temperature_f):
-#     query = "INSERT INTO books(title,isbn) " \
+#     query = "INSERT INTO waether_history(city,teme) " \
 #             "VALUES(%s,%s)"
 #     args = (title, isbn)
  
@@ -61,4 +68,3 @@ for city in cities:
 #     finally:
 #         cursor.close()
 #         conn.close()
-
